29d9e729cf35f8791b01c644aefa2849346f740b
NureHerasymovaVladyslava <128616324+NureHerasymovaVladyslava@users.noreply.github.com>
2024-12-23 12:37:48 +0200

    Added created code for lab 5

---
+++ Task5/arkpz-pzpi-22-4-herasymova-vladyslava-task5/DAL/DatabaseAdminManager.cs
@@ -0,0 +1,106 @@
+﻿using Core.Models;
+using Dapper;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class DatabaseAdminManager
+    {
+        private readonly string _connectionString;
+        private readonly string _databaseName;
+
+        public DatabaseAdminManager(IConfiguration config)
+        {
+            _connectionString = config.GetConnectionString("MasterConnectionString");
+            _databaseName = config.GetConnectionString("TargetDatabaseName");
+        }
+
+        public async Task BackupDatabaseAsync(string backupPath)
+        {
+            using var connection = new SqlConnection(_connectionString);
+
+            var sql = $@"BACKUP DATABASE [{_databaseName}]
+            TO DISK = @backupPath
+            WITH FORMAT, INIT;";
+
+            await connection.ExecuteAsync(sql, new { backupPath });
+        }
+
+        public async Task RestoreDatabaseAsync(string backupPath)
+        {
+            using var connection = new SqlConnection(_connectionString);
+
+            var checkDatabaseExistsSql = $@"
+            SELECT COUNT(*) 
+            FROM sys.databases 
+            WHERE name = '{_databaseName}'";
+
+            var disableConnectionsSql = $@"USE [master];
+            ALTER DATABASE [{_databaseName}] 
+            SET SINGLE_USER WITH ROLLBACK IMMEDIATE;";
+
+            var restoreSql = $@"USE [master];
+            RESTORE DATABASE [{_databaseName}]
+            FROM DISK = @backupPath
+            WITH REPLACE;";
+
+            var enableConnectionsSql = $@"USE [master];
+            ALTER DATABASE [{_databaseName}] 
+            SET MULTI_USER;";
+
+            var databaseExists = await connection
+                .ExecuteScalarAsync<int>(checkDatabaseExistsSql) > 0;
+
+            if (databaseExists)
+            {
+                await connection.ExecuteAsync(disableConnectionsSql);
+            }
+
+            await connection.ExecuteAsync(restoreSql, new { backupPath });
+
+            if (databaseExists)
+            {
+                await connection.ExecuteAsync(enableConnectionsSql);
+            }
+        }
+
+        public async Task<bool> SetupDatabase(string backupFilePath)
+        {
+            if (!File.Exists(backupFilePath))
+            {
+                throw new FileNotFoundException
+                    ("Database backup file not found.", backupFilePath);
+            }
+
+            using var connection = new SqlConnection(_connectionString);
+
+            var checkDatabaseExistsSql = $@"
+            SELECT COUNT(*) 
+            FROM sys.databases 
+            WHERE name = '{_databaseName}'";
+
+            var databaseExists = await connection
+                .ExecuteScalarAsync<int>(checkDatabaseExistsSql) > 0;
+
+            if (databaseExists)
+            {
+                return false;
+            }
+
+            var restoreSql = $@"
+            RESTORE DATABASE [{_databaseName}]
+            FROM DISK = @backupFilePath
+            WITH REPLACE;";
+
+            await connection.ExecuteAsync(restoreSql, new { backupFilePath });
+
+            return true;
+        }
+    }
+}
---
+++ Task5/arkpz-pzpi-22-4-herasymova-vladyslava-task5/WebAPI/Controllers/SetupController.cs
@@ -0,0 +1,94 @@
+﻿using Core.Helpers;
+using Core.Models;
+using DAL;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Cryptography;
+using System.Text;
+using WebAPI.Managers;
+using WebAPI.Models.AppUser;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SetupController : ControllerBase
+    {
+        private readonly DatabaseAdminManager _databaseAdminManager;
+        private readonly AppUserRepository _userRepository;
+        private readonly UserRoleRepository _roleRepository;
+        private readonly EmailManager _emailManager;
+
+        public SetupController(DatabaseAdminManager databaseAdminManager, 
+            AppUserRepository userRepository, UserRoleRepository roleRepository, 
+            EmailManager emailManager)
+        {
+            _databaseAdminManager = databaseAdminManager;
+            _userRepository = userRepository;
+            _roleRepository = roleRepository;
+            _emailManager = emailManager;
+        }
+
+        [HttpPost("database")]
+        public async Task<IActionResult> SetupDatabase(string backupFilePath)
+        {
+            try
+            {
+                var result = await _databaseAdminManager.SetupDatabase(backupFilePath);
+
+                return result ? Ok() : BadRequest();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+
+        [HttpPost("first-user")]
+        public async Task<IActionResult> CreateFirstUser([FromBody] CreateFirstUserModel model)
+        {
+            try
+            {
+                var users = await _userRepository.GetAllAsync();
+                if (users.Any())
+                {
+                    return BadRequest();
+                }
+
+                var appUser = new AppUser();
+                appUser.MapFrom(model);
+
+                var userRole = await _roleRepository.GetRoleByName(UserRoleManager.RoleAdmin);
+                if (userRole == null)
+                {
+                    return NotFound();
+                }
+
+                appUser.RoleId = userRole.Id;
+
+                var tempPassword = PasswordManager.GenerateTemporaryPassword();
+                appUser.PasswordHash = HashPassword(tempPassword);
+
+                var result = await _userRepository.CreateAsync(appUser);
+
+                var emailResult = await _emailManager
+                    .SendNewUserEmail(appUser.FullName, tempPassword, appUser.EmailAddress);
+
+                return emailResult ? Ok(result) : StatusCode(
+                    StatusCodes.Status500InternalServerError, "Failed to send an email");
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+
+        private string HashPassword(string password)
+        {
+            using var sha256 = SHA256.Create();
+            var passwordBytes = Encoding.UTF8.GetBytes(password);
+            var hashBytes = sha256.ComputeHash(passwordBytes);
+            return Convert.ToBase64String(hashBytes);
+        }
+    }
+}
